33f10a04b66a54295f6cecba1ca96fe3766dd687
AnthonyLeyasi <leyasianthony@icloud.com>
2023-07-24 21:15:35 -0700

    push

--- flask_app/controller/user.py
+++ flask_app/controller/user.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect,session
+from flask import render_template, request, redirect,session,url_for
 from flask_app.models.users import User
 from flask_app.models.matches import Match
 from flask_app.models.messages import Message
@@ -14,7 +14,9 @@ import googlemaps
 
 API_KEY = 'AIzaSyAD3X-hrcyk92c6pTIsp6iQE-camaL-mWw'
 map_client = googlemaps.Client(API_KEY)
+
 # map_client.geocode()
+
 UPLOAD_FOLDER = 'flask_app/static/uploaded_images'
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
 
@@ -164,3 +166,26 @@ def edit_user_info():
 
 
 
+#display imgs
+
+# @app.config[''] == UPLOAD_FOLDER
+# def allowed_file(filename):
+#     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+    
+# @app.route('/ltg/submit', methods=['POST'])
+# def create_ltg():
+#     # Your other code...
+#     if 'image' not in request.files:
+#         flash('No file part')
+#         return redirect('/ltg/form')
+#     image = request.files['image']
+#     print(image)
+#     if image.filename == '':
+#         flash('No selected file')
+#         return redirect('/ltg/form')
+#     if image and allowed_file(image.filename):
+#         filename = secure_filename(image.filename)
+#         print("Filename: ", filename)
+#         image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
